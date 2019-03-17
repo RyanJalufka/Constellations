4fbdccf594dbdc304a6a18d5ac851a1617a00d13
Ryan Jalufka <ryanjalufka@gmail.com>
2019-03-17 13:47:06 -0600

    chore: adding dependencies and setting up redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = 'fix this'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 
