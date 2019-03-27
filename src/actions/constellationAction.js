8871825ddb409a1cc54c5855533aa9ec079606e0
Kristian Saenz <kristian.nicole94@yahoo.com>
2019-03-27 22:35:49 +0100

    chore: removing console log

--- src/actions/constellationAction.js
+++ src/actions/constellationAction.js
@@ -2,7 +2,6 @@ import { SET_CURRENT_CONSTELLATION } from './types';
 
 export const setCurrentConstellation = (constellation, dispatch) => {
   return(dispatch) => {
-    console.log('SET_CURRENT_MONTH: ', constellation);
     dispatch({ type: SET_CURRENT_CONSTELLATION, payload: constellation});
   }
 }
