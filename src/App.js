220a5b39c6fe7b84c0984cad1816134ee4fb828f
Ryan Jalufka <ryanjalufka@gmail.com>
2019-03-27 20:59:57 +0100

    chore: fixed typo

--- src/App.js
+++ src/App.js
@@ -6,15 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
       <Header />
       <MonthSelect />
       {/*
       <ConstellationSelect />
       <ConstellationCard />  */}
-=======
-      <h1>hAllo there :D</h1>
->>>>>>> 8da5bb8ceab89cbe648275f90a5e0015bde96be3
       </div>
     );
   }
