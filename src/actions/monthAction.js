2a56df18a65e7cebe7c5980a9e7150dcc9d214ad
Kristian Saenz <kristian.nicole94@yahoo.com>
2019-03-27 22:36:27 +0100

    chore: removing console log

--- src/actions/monthAction.js
+++ src/actions/monthAction.js
@@ -2,7 +2,6 @@ import { SET_CURRENT_MONTH } from './types';
 
 export const setCurrentMonth = (month, dispatch) => {
   return(dispatch) => {
-    console.log('SET_CURRENT_MONTH: ', month);
     dispatch({ type: SET_CURRENT_MONTH, payload: month});
   }
 }
